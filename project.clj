09a297eb1e0e3d195007c6acd91c1515a6afe06a
Moe Aboulkheir <moe.aboulkheir@gmail.com>
2015-07-14 20:02:02 +0100

    1.0.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.nervous/eulalie "0.5.2"
+(defproject io.nervous/eulalie "1.0.0-SNAPSHOT"
   :description "Asynchronous, pure-Clojure AWS client"
   :url "https://github.com/nervous-systems/eulalie"
   :license {:name "Unlicense" :url "http://unlicense.org/UNLICENSE"}
